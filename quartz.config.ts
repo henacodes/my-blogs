9059d884cf2678b5d2caef4c598fcd5eddbda230
Kirakos <henacodes@gmail.com>
2026-03-30 01:30:15 +0300

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "blog.kirakos.dev",
+    baseUrl: "blog.kirakos.tech",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
